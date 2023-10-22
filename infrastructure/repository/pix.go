65c7c8f74b5b3e1dba904de67a1f1b9e4a24fa0b
unknown <alexandre8603@gmail.com>
2023-10-21 21:56:50 -0300

    Model adjustments

--- infrastructure/repository/pix.go
+++ infrastructure/repository/pix.go
@@ -1,11 +1,57 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+	"github.com/XandaoTP/golandcodepix/domain/model"
+	"gorm.io/gorm"
+)
 
 type PixKeyRepositoryDb struct {
 	Db *gorm.DB
 }
 
-func (r PixKeyRepositoryDb) AddBank(bank *model) {
+func (r PixKeyRepositoryDb) AddBank(bank *model.Bank) error {
+	err := r.Db.Create(bank).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
+func (r PixKeyRepositoryDb) AddAccount(account *model.Account) error {
+	err := r.Db.Create(account).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r PixKeyRepositoryDb) RegisterKey(pixKey *model.PixKey) (*model.PixKey, error) {
+	err := r.Db.Create(pixKey).Error
+	if err != nil {
+		return nil, err
+	}
+	return pixKey, nil
+}
+
+func (r PixKeyRepositoryDb) FindKeyById(key string, kind string) (*model.PixKey, error) {
+	var pixKey model.PixKey
+
+	r.Db.Preload("Account.Bank").First(&pixkey, "kind = ? and key = ?", kind, key)
+
+	if pixKey.ID == "" {
+		return nil, fmt.Errorf("no key was found")
+	}
+	return &pixKey, nil
+}
+
+func (r PixKeyRepositoryDb) FindKeyById(key string, kind string) (*model.PixKey, error) {
+	var pixKey model.PixKey
+
+	r.Db.Preload("Account.Bank").First(&pixkey, "kind = ? and key = ?", kind, key)
+
+	if pixKey.ID == "" {
+		return nil, fmt.Errorf("no key was found")
+	}
+	return &pixKey, nil
 }
